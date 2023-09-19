bc126e778b64fb110b3e0594187009b5a3f62e15
Daniel Reinosa Luque <114745664+DReino03@users.noreply.github.com>
2023-09-19 09:42:36 +0200

    Add files via upload

--- Renaming_files.py
+++ Renaming_files.py
@@ -1,10 +1,17 @@
+import os
+
 def rename_file():
     exit = False
     while exit == True:
             ruta = input(str("Indica la ruta del directori que vols treballar:"))
+            # os.chdir lleva a la ruta especificada
+            os.chdir(ruta)
+
             fitxer = input(str("Indica el fitxer que vols modificar el nom:"))
+            open(fitxer,"w")
+
             rename_file = input(str("Indica quin nom vols fer servir"))
             
-
             
+
     
